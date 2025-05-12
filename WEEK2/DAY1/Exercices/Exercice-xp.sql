4aa8ea73c3f62903fd06848e1c5b2b41ca42f6a4
unknown <hamza.aboulhana4@gmail.com>
2025-05-12 23:02:32 +0100

    done all exercices

--- WEEK2/DAY1/Exercices/Exercice-xp.sql
+++ WEEK2/DAY1/Exercices/Exercice-xp.sql
@@ -38,3 +38,51 @@ select * from customers where last_name='Smith';
 select * from customers where last_name='Jones';
 
 select * from customers where first_name!='Scott';
+
+--Exercice-Xp-Plus :
+
+create database bootcamp;
+
+create table students(
+	 id SERIAL primary key,
+	 last_name varchar(100) Not null,
+	 first_name varchar(100) not null,
+	 birth_date date not null);
+
+DROP TABLE students
+
+insert into students (last_name,first_name,birth_date) 
+values ('Benichou','Marc','1998/11/02'),
+       ('Cohen','Yoan','2010/12/03'),
+	   ('Benichou','Lea','1987/07/27'),
+	   ('Dux','Amelia','1996/04/07'),
+	   ('Grez','David','2003/06/14'),
+	   ('Simpson','Omer','1980/10/03');
+
+select first_name,last_name from students;
+
+select first_name, last_name from students where id = 2 ; 
+
+select last_name, first_name from students where last_name='Benichou' and first_name='Marc';
+
+select last_name, first_name from students where last_name='Benichou' or first_name='Marc';
+
+select first_name, last_name from students where first_name like '%a%' ;
+
+select first_name, last_name from students where first_name like 'a%' ;
+
+select first_name, last_name from students where first_name like '%a' ;
+
+SELECT first_name, last_name FROM students WHERE first_name ~ '.a.$';
+
+SELECT first_name, last_name FROM students WHERE id IN (1, 3);
+
+SELECT * FROM students WHERE birth_date >= '2000-01-01';
+
+--exercice Xp Gold :
+
+SELECT first_name, last_name, birth_date FROM students ORDER BY last_name ASC LIMIT 4;
+
+SELECT first_name, last_name, birth_date FROM students ORDER BY birth_date DESC LIMIT 1;
+
+SELECT first_name, last_name, birth_date FROM students OFFSET 2 LIMIT 3;
