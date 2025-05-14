88b27e8e6a83b8b6339143bc2cfc8300367fa8ab
unknown <hamza.aboulhana4@gmail.com>
2025-05-14 10:49:27 +0100

    Done

---
+++ WEEK2/DAY2/Daily-challenge/Sql-puzzle.sql
@@ -0,0 +1,13 @@
+--Q1. What will be the OUTPUT of the following statement?
+--Résultat attendu :
+0
+
+--Q2. What will be the OUTPUT of the following statement?
+--Résultat attendu :
+2
+--Q3. What will be the OUTPUT of the following statement?
+--Résultat attendu :
+0
+--Q4. What will be the OUTPUT of the following statement?
+--Résultat attendu :
+2
